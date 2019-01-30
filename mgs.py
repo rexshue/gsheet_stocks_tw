fd44c6a76b95c1c19f02ba7f8184a6a618e71014
rexshue <rexshue@gmail.com>
2019-01-30 20:51:12 +0800

    first commit

---
+++ mgs.py
@@ -0,0 +1,75 @@
+# -*- coding: utf-8 -*-
+
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+from myStock import get_stock_dividend
+from myStock import get_stock_price
+import time
+
+def auth_gss_client(path, scopes):
+    credentials = ServiceAccountCredentials.from_json_keyfile_name(path, scopes)
+    return gspread.authorize(credentials)
+
+def get_sheet_value(sheet, row, col):
+    time.sleep(1.1)
+    return sheet.cell(row,col).value
+
+def set_sheet_value(sheet, row, col, value):
+    time.sleep(1.1)
+    sheet.update_cell(row,col,value)
+
+def set_sheet_values(sheet, row, col, values):
+    alpha='ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    preA=alpha[col-1]+str(row)
+    m=col+len(values)-2
+    j=m/len(alpha)
+    k=m%len(alpha)
+    if j>0:
+        preB=alpha[j-1]+alpha[k]+str(row)
+    else:
+        preB=alpha[col+len(values)-2]+str(row)
+    label=preA+':'+preB
+    cell_list=sheet.range(label)
+    for c,v in zip(cell_list, values):
+        c.value = v
+    time.sleep(1.1)
+    sheet.update_cells(cell_list)
+
+def update_divident(no,rows):
+    results = get_stock_dividend(no)
+    colIndex = 14
+    cell_value = []
+    for result in results:
+        cell_value.append(result[1])
+        cell_value.append(result[2])
+        #set_sheet_value(wks.sheet1,lineIndex, colIndex, result[1])
+        #colIndex += 1
+        #set_sheet_value(wks.sheet1,lineIndex, colIndex, result[2])
+        #colIndex += 1
+    set_sheet_values(wks.sheet1,rows,colIndex, cell_value)
+
+auth_json_path = 'auth.json'
+gss_scopes = ['https://spreadsheets.google.com/feeds']
+
+gss_client = auth_gss_client(auth_json_path, gss_scopes)
+s_key='1iQyTxY6WN80xYSTURKXmH-7twnkH9Vk75fn_cGs-x7c'
+#s_key=open('spreadsheet_key','r').read()
+
+wks = gss_client.open_by_key(s_key)
+#print(get_sheet_value(wks.sheet1, 2, 1))
+stockNo = 1
+lineIndex = 2
+run = True
+while run:
+    stockNo = get_sheet_value(wks.sheet1, lineIndex ,1)
+    if stockNo != '':
+        print('Get Stock '+str(stockNo)+'\n')
+        set_sheet_value(wks.sheet1, lineIndex, 3, get_stock_price(stockNo))
+        divValue = get_sheet_value(wks.sheet1, lineIndex, 14)
+        if divValue == '':
+            update_divident(stockNo, lineIndex)
+        time.sleep(1)
+        lineIndex += 1
+    else:
+        print('Bye!\n')
+        run = False
---
+++ myStock.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+from bs4 import BeautifulSoup
+import requests
+import sys
+import io
+import tempfile
+import time
+
+def get_stock_dividend(no):
+    url ='https://tw.stock.yahoo.com/d/s/dividend_' + str(no) +'.html'
+    req = requests.get(url, timeout=5, allow_redirects=True)
+    tmpName = tempfile.gettempdir() + '\\t2.html'
+    open(tmpName, 'wb').write(req.content)
+    results=[]
+    with io.open(tmpName,'r') as f:
+        content = f.read()
+        soup = BeautifulSoup(content, 'lxml')
+        trs = soup.find_all('tr', {'bgcolor':'#FFFFFF'})
+        for tr in trs:
+            tds = tr.find_all('td')
+            result=[]
+            for td in tds:
+                result.append(float(td.text))
+            results.append(result)
+    return results
+
+def get_stock_price(no):
+    url ='https://tw.stock.yahoo.com/q/ts?s='+str(no)
+    req = requests.get(url, timeout=5, allow_redirects=True)
+    tmpName = tempfile.gettempdir() + '\\t1.html'
+    open(tmpName,'wb').write(req.content)
+    result=0.0
+    with io.open(tmpName,'r') as f:
+        content = f.read()
+        soup = BeautifulSoup(content, 'lxml')
+        trs = soup.find_all('tr', {'bgcolor':'#ffffff'})
+        tds = trs[0].find_all('td')
+        result= float(tds[3].text)
+    return result;
+#print(get_stock_dividend(2892))
+#time.sleep(1)
+#print(get_stock_dividend(2834))
+#print(get_stock_price(2892))
